efd58d0713ed74d5f00a32f8515f4b17bfb79b99
samnart <tettey1234567@gmail.com>
2023-12-18 14:55:20 +0100

    bookingController.js

---
+++ firstSem/contemporary/micro/controllers/bookingController.js
@@ -0,0 +1,30 @@
+// controllers/bookingController.js
+
+const Booking = require('../models/Booking');
+
+const createBooking = async (req, res) => {
+    try {
+        const { accommodationId, startDate, endDate } = req.body;
+
+        // create a booking in the database
+        const booking = await Booking.create({ accommodationId, startDate, endDate });
+
+        res.json({ booking });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const getBookings = async (req, res) => {
+    try {
+        // retrieve bookings from database
+        const bookings = await Booking.find();
+        res.json({ bookings });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+module.exports = { createBooking, getBookings };
