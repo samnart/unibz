05fca4ff868ba807ceca8bf4caa814bf2d3cc9e5
samnart <tettey1234567@gmail.com>
2023-12-18 13:34:34 +0100

    modified micro

--- firstSem/contemporary/micro/apartment-microservice/config/database.js
+++
@@ -1,8 +0,0 @@
-// config/database.js
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/search_microservice_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
