480c3c7045b94aff7a080b4f2824758ad4792388
samnart <tettey1234567@gmail.com>
2023-12-21 18:45:26 +0100

    modified unibz dir.

--- firstSem/contemporary/GradeAi/src/main/Grade.java
+++ firstSem/contemporary/GradeAi/src/main/Grade.java
@@ -1,2 +1,26 @@
+package src.main;
+
 public class Grade {
+    private final boolean isApproved;
+    private final int numericGrade;
+    private final boolean isCumLaude;
+
+    public Grade(boolean isApproved, int numericGrade, boolean isCumLaude) {
+        this.isApproved = isApproved;
+        this.numericGrade = numericGrade;
+        this.isCumLaude = isCumLaude;
+    }
+
+    public boolean isApproved() {
+        return isApproved;
+    }
+
+    public int getNumericGrade() {
+        return numericGrade;
+    }
+
+    public boolean isCumLaude() {
+        return isCumLaude;
+    }
 }
+
--- firstSem/contemporary/GradeAi/src/main/GradeCalculator.java
+++ firstSem/contemporary/GradeAi/src/main/GradeCalculator.java
@@ -1,2 +1,31 @@
+package src.main;
+
 public class GradeCalculator {
+
+    public static Grade calculateGrade(int labPoints, float examGrade) throws Exception {
+        validateInput(labPoints, examGrade);
+
+        int totalPoints = calculateTotalPoints(labPoints);
+        int numericGrade = calculateNumericGrade(totalPoints);
+        boolean isApproved = numericGrade >= 18;
+        boolean isCumLaude = numericGrade == 30 && labPoints > 50;
+
+        return new Grade(isApproved, numericGrade, isCumLaude);
+    }
+
+    private static void validateInput(int labPoints, float examGrade) throws Exception {
+        if (examGrade < 0 || examGrade > 10 || labPoints < 0) {
+            throw new Exception("Invalid input values");
+        }
+    }
+
+    private static int calculateTotalPoints(int labPoints) {
+        int extraPoints = Math.min((labPoints - 50) / 5, 3);
+        return Math.min(50, labPoints) + extraPoints;
+    }
+
+    private static int calculateNumericGrade(int totalPoints) {
+        return Math.min(30, totalPoints * 2);
+    }
 }
+
