275bcf3116b8361fb7bbd616e6f706e6902c1510
samnart <tettey1234567@gmail.com>
2023-12-26 15:14:17 +0100

    modified searchController.js to make HTTP GET request to Apartment Microservice

--- firstSem/contemporary/hotel-system/search-microservice/controllers/searchController.js
+++ firstSem/contemporary/hotel-system/search-microservice/controllers/searchController.js
@@ -1,13 +1,22 @@
-// search-microservice/controllers/searchController.js
+const axios = require('axios');
 
-const Accommodation = require('../models/Accommodation');
+const getAccommodationsWithDetails = async () => {
+  try {
+    // Make a request to the Apartment Microservice to get detailed information about accommodations
+    const response = await axios.get('http://localhost:3002/api/apartments');
+    return response.data;
+  } catch (error) {
+    console.error('Error communicating with Apartment Microservice', error);
+    throw error;
+  }
+};
 
 const searchHandler = async (req, res) => {
   try {
-    const accommodations = await Accommodation.find({ availability: true });
+    const accommodations = await getAccommodationsWithDetails();
     res.json({ accommodations });
   } catch (error) {
-    console.error(error);
+    console.error('Error processing search', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
