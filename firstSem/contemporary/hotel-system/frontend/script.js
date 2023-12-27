fbf1676756b57db7f6c446cf8ff3e8e786463ff6
samnart <tettey1234567@gmail.com>
2023-12-27 14:06:51 +0100

    hmmm, js

---
+++ firstSem/contemporary/hotel-system/frontend/script.js
@@ -0,0 +1,60 @@
+// frontend/script.js
+
+document.getElementById('searchForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const location = document.getElementById('location').value;
+
+    try {
+        // Make API request to search-microservice
+        const searchResponse = await fetch('http://localhost:3001/api/search?location=' + location);
+        const searchData = await searchResponse.json();
+
+        // Update #searchResults with the search results
+        const searchResultsContainer = document.getElementById('searchResults');
+        searchResultsContainer.innerHTML = ''; // Clear previous results
+
+        if (searchData && searchData.results) {
+            searchData.results.forEach(result => {
+                const resultElement = document.createElement('div');
+                resultElement.textContent = result.name; // You can customize this based on your search result structure
+                searchResultsContainer.appendChild(resultElement);
+            });
+        }
+
+        // Make API request to apartment-microservice
+        const apartmentResponse = await fetch('http://localhost:3002/api/apartments');
+        const apartmentData = await apartmentResponse.json();
+
+        // Update #apartmentsWithBookings with apartments and booking information
+        const apartmentsWithBookingsContainer = document.getElementById('apartmentsWithBookings');
+        apartmentsWithBookingsContainer.innerHTML = ''; // Clear previous results
+
+        if (apartmentData && apartmentData.apartmentsWithBookings) {
+            apartmentData.apartmentsWithBookings.forEach(apartment => {
+                const apartmentCard = document.createElement('div');
+                apartmentCard.className = 'apartmentCard';
+
+                const heading = document.createElement('h2');
+                heading.textContent = apartment.name;
+                apartmentCard.appendChild(heading);
+
+                const locationPara = document.createElement('p');
+                locationPara.textContent = `Location: ${apartment.location}`;
+                apartmentCard.appendChild(locationPara);
+
+                const pricePara = document.createElement('p');
+                pricePara.textContent = `Price: ${apartment.price}`;
+                apartmentCard.appendChild(pricePara);
+
+                const bookingPara = document.createElement('p');
+                bookingPara.textContent = `Booking: ${apartment.booking ? 'Booked' : 'Available'}`;
+                apartmentCard.appendChild(bookingPara);
+
+                apartmentsWithBookingsContainer.appendChild(apartmentCard);
+            });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
