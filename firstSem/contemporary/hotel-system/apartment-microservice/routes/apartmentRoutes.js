f3c4830316e7568bfc1bf0c5a7fb3a230a3f9417
samnart <tettey1234567@gmail.com>
2023-12-28 17:15:07 +0100

    apartment-microservice updated

--- firstSem/contemporary/hotel-system/apartment-microservice/routes/apartmentRoutes.js
+++ firstSem/contemporary/hotel-system/apartment-microservice/routes/apartmentRoutes.js
@@ -1,9 +1,48 @@
 // apartment-microservice/routes/apartmentRoutes.js
 
+// const express = require('express');
+// const router = express.Router();
+// const apartmentController = require('../controllers/apartmentController');
+
+// const Apartment = require('../models/Apartment');
+
+// router.get('/apartments', apartmentController.getApartments);
+
+// module.exports = router;
+
+
+
+// apartment-microservice/routes/apartmentRoutes.js
+
 const express = require('express');
 const router = express.Router();
-const apartmentController = require('../controllers/apartmentController');
+const Apartment = require('../models/Apartment');
 
-router.get('/apartments', apartmentController.getApartments);
+// GET request to retrieve apartments
+router.get('/apartments', async (req, res) => {
+    // Your existing code to retrieve apartments...
+});
+
+// POST request to add a new apartment
+router.post('/apartments', async (req, res) => {
+    try {
+        const { name, location, price } = req.body;
+
+        // Create a new apartment instance
+        const newApartment = new Apartment({
+            name,
+            location,
+            price,
+        });
+
+        // Save the apartment to the database
+        await newApartment.save();
+
+        res.status(201).json({ message: 'Apartment added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
