908034a98200e97d1f0f6ff9c7908f2393941b4b
samnart <tettey1234567@gmail.com>
2024-01-13 23:34:10 +0100

    update  to ue rabbitmq

--- firstSem/contemporary/hotel-system/apartment-microservice/server.js
+++ firstSem/contemporary/hotel-system/apartment-microservice/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const apartmentRoutes = require('./routes/apartmentRoutes');
 const getDatabaseConfig = require('./config/database');
+const amqp = require('amqplib');
+
+const { rabbitmqConfig } = require('./config/database')
 
 const cors = require('cors');
 
@@ -14,10 +17,40 @@ const apartmentMicroserviceDbConfig = getDatabaseConfig('apartment_microservice'
 mongoose.connect(apartmentMicroserviceDbConfig.database, apartmentMicroserviceDbConfig.options);
 
 app.use(express.json());
-app.use('/api', apartmentRoutes);
 
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
-  console.log(`Apartment Microservice is running on port ${port}`);
-});
+// Connect to RabbitMQ
+const startMicroservice = async () => {
+  try {
+    const connection = await amqp.connect(rabbitmqConfig.url);
+    const channel = await connection.createChannel();
+    const queue = rabbitmqConfig.queueName;
+
+    // Assert the queue
+    await channel.assertQueue(queue, { durable: false });
+
+    // Use the channel in the routes
+    app.use('/api', apartmentRoutes(channel));
+
+    const port = process.env.PORT || 3002;
+    app.listen(port, () => {
+      console.log(`Apartment Microservice is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log('Error starting Apartment Microservice', error);
+  }
+}
+
+startMicroservice();
+
+
+
+
+
+
+// app.use('/api', apartmentRoutes);
+
+// const port = process.env.PORT || 3002;
+// app.listen(port, () => {
+//   console.log(`Apartment Microservice is running on port ${port}`);
+// });
 
