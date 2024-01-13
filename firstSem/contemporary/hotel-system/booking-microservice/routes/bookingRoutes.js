125f887611618e5c3aa78dcec11d7167b0732091
samnart <tettey1234567@gmail.com>
2024-01-14 00:04:54 +0100

    update booking-microservice/routes/bookingRoutes.js to use rabbitmq

--- firstSem/contemporary/hotel-system/booking-microservice/routes/bookingRoutes.js
+++ firstSem/contemporary/hotel-system/booking-microservice/routes/bookingRoutes.js
@@ -4,7 +4,32 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-router.post('/bookings', bookingController.createBooking);
-router.get('/bookings', bookingController.getBookings);
+module.exports = (channel) => {
+    router.post('/bookings', async (req, res) => {
+        try {
+            const { accommodationId, startDate, endDate } = req.body;
+            const booking = {
+                accommodationId,
+                startDate,
+                endDate,
+            };
 
-module.exports = router;
+            // Send a message to RabbitMQ when a new booking is created
+            channel.sendToQueue('booking_queue', Buffer.from(JSON.stringify(booking)));
+
+            res.json({ booking });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+    router.get('/bookings', bookingController.getBookings);
+
+    return router;
+};
+
+// router.post('/bookings', bookingController.createBooking);
+// router.get('/bookings', bookingController.getBookings);
+
+// module.exports = router;
