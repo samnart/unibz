116e3eee7f28feae253a657fcc60f793fd92ee36
samnart <tettey1234567@gmail.com>
2024-01-13 23:44:00 +0100

    update  to use rabbitmq

--- firstSem/contemporary/hotel-system/booking-microservice/server.js
+++ firstSem/contemporary/hotel-system/booking-microservice/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bookingRoutes = require('./routes/bookingRoutes');
 const getDatabaseConfig = require('./config/database');
+const amqp = require('amqplib');
+
+const { rabbitmqConfig } = require('./config/database');
 
 const app = express();
 
@@ -11,9 +14,37 @@ const bookingMicroserviceDbConfig = getDatabaseConfig('booking_microservice');
 mongoose.connect(bookingMicroserviceDbConfig.database, bookingMicroserviceDbConfig.options);
 
 app.use(express.json());
-app.use('/api', bookingRoutes);
 
-const port = process.env.PORT || 3003;
-app.listen(port, () => {
-  console.log(`Booking Microservice is running on port ${port}`);
-});
+// Connect to RabbitMQ
+const startMicroservice = async () => {
+  try {
+    const connection = await amqp.connect(rabbitmqConfig.url);
+    const channel = await connection.createChannel();
+    const queue = rabbitmqConfig.queueName;
+
+    // Assert the queue
+    await channel.assertQueue(queue, { durable: false });
+
+    // Use the channel in the routes
+    app.use('/api', bookingRoutes(channel));
+
+    const port = process.env.PORT || 3003;
+    app.listen(port, () => {
+      console.log(`Booking Microservice is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log('Error starting Booking Microservice', error);
+  }
+};
+
+startMicroservice();
+
+
+
+
+// app.use('/api', bookingRoutes);
+
+// const port = process.env.PORT || 3003;
+// app.listen(port, () => {
+//   console.log(`Booking Microservice is running on port ${port}`);
+// });
