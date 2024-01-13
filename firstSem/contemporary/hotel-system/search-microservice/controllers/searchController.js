f95c0c138a44e6a14ccf7f0c1a3118f76bcc3e3e
samnart <tettey1234567@gmail.com>
2024-01-13 23:23:48 +0100

    corrected and deleted unwanted codes from the search-microservice

--- firstSem/contemporary/hotel-system/search-microservice/controllers/searchController.js
+++ firstSem/contemporary/hotel-system/search-microservice/controllers/searchController.js
@@ -1,19 +1,4 @@
 // // search-microservice/controllers/searchController.js
-
-// const Accommodation = require('../models/Accommodation');
-
-// const searchHandler = async (req, res) => {
-//   try {
-//     const accommodations = await Accommodation.find({ availability: true });
-//     res.json({ accommodations });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
-// module.exports = { searchHandler };
-
 const axios = require('axios');
 
 const getAccommodationsWithDetails = async () => {
